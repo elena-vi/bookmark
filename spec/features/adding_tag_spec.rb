0ff833db2a89350e550352db3ce1390984de7d8f
Elena <elena.vilimaite@gmail.com>
2016-06-16 10:14:41 +0100

    multiple tags feature

--- spec/features/adding_tag_spec.rb
+++ spec/features/adding_tag_spec.rb
@@ -13,3 +13,19 @@ feature 'Adding tags' do
   end
 
 end
+
+
+feature 'Adding multiple tags' do
+
+  scenario 'I can amultiple tags to a new link' do
+    visit '/links/new'
+    fill_in 'url',   with: 'http://www.makersacademy.com/'
+    fill_in 'title', with: 'Makers Academy'
+    fill_in 'tags',  with: 'education ruby'
+
+    click_button 'Add New Link'
+    link = Link.first
+    expect(link.tags.map(&:name)).to include('education ruby')
+  end
+
+end
